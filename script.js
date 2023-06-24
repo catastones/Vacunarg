6d87835f1be435688deb128891028d29b79f3f8e
Nicolas <nicolaszelarayan.ez@gmail.com>
2023-06-24 07:45:31 -0300

    integramos api de login

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-
-function login() {
+function login(event) {
+    event.preventDefault(); // prevent form from submitting
     const username = document.querySelector('#username').value;
     const password = document.querySelector('#password').value;
   
@@ -12,8 +12,7 @@ function login() {
     })
       .then((response) => {
         if (response.status === 200) {
-          alert('Login exitoso');
-          //window.location.href = '/index2.html'; // redirige a index2.html si la respuesta es 200
+          window.location.href = '/index2.html'; // redirige a index2.html si la respuesta es 200
         } else {
           console.error('Error:', response.status);
         }
