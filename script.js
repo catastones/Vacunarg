b8d05617918316a6074dff2acbcae0679d1ec45f
Marcelo Agustín Gómez Aranda <39355734@alumnos.frsr.utn.edu.ar>
2023-06-24 06:00:05 -0300

    cambios en el script

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-import Swal from 'sweetalert2';
 
-
-document.querySelector("button[type='submit']").addEventListener("click", function(e) {
-    e.preventDefault();
-    Swal.fire({
-        icon: 'success',
-        title: '¡Hecho!',
-        text: 'Pronto llegará un correo para restablecer tu contraseña',
-        confirmButtonText: 'Ok'
-    }).then((result) => {
-        if (result.isConfirmed) {
-            window.location.href = "index.html";
-        }
-    })
-});
